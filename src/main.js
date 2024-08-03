bbd4eb203e59b689ef1c6ef8fdce121ceb2f6125
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-08-03 19:53:09 +0300

    bug fix icon

--- src/main.js
+++ src/main.js
@@ -41,7 +41,7 @@ form.addEventListener('submit', event => {
       if (error.message == 'No images found') {
         iziToast.warning({
           icon: 'svg',
-          iconUrl: '/src/img/Warning.svg',
+          iconUrl: './img/warning.svg',
           title: 'Warning',
           position: 'topRight',
           message: 'Sorry, there are no images matching your search query. Please try again!',
